91626f60a9956799a8c42ca8ef88c7256e034c2b
marwill206 <laptopmerhino22@hotmail.com>
2025-05-09 09:33:40 +0200

    setup

---
+++ loveapp/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
+const nextConfig = withPWA({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+});
+
+export default nextConfig;
