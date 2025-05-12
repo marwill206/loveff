797dac1a28501da19d4a5dc8e812822ea1863df7
marwill206 <laptopmerhino22@hotmail.com>
2025-05-12 10:36:01 +0200

    Initialize my PWA project with Vite and React
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to the public directory.
    - Created App.css for styling the application layout and animations.
    - Developed App.jsx component with basic functionality and logos.
    - Included React logo SVG in the assets directory.
    - Established global styles in index.css for consistent theming.
    - Set up main.jsx as the entry point for the React application.
    - Configured Vite with a basic vite.config.js file for React support.

--- loveapp/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-import withPWA from "next-pwa";
-const nextConfig = withPWA({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-});
-
-export default nextConfig;
